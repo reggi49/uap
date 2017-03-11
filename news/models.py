59737c99034b654dedeef1aa609e4e7f9c85d182
reggi49 <reggimuhamad@yahoo.com>
2017-03-11 23:03:50 +0700

    update 1

---
+++ news/models.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Feed(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    is_active = models.BooleanField(default=False)
+
+class Article(models.Model):
+    feed = models.ForeignKey(feed)
+    title = models.CharField(max_length=200)
+    url = models.URLField
+    description = models.TexxField()
+    publication_date = models.DateTimeField()
